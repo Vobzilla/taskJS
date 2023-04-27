417278c103a7685dcd15d2a08f066bd3de47285a
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2023-04-27 15:11:26 +0200

    constructors task

---
+++ constructors.js
@@ -0,0 +1,70 @@
+/** @format */
+
+// const Car = function ({ brand, model, price } = {}) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+// };
+
+// const myCar = new Car({
+//   brand: "Audi",
+//   model: "Q3",
+//   price: 35000,
+// });
+// console.log(myCar);
+
+// myCar.changePrice(1000000);
+
+// const myCar2 = new Car({
+//   brand: "Mazda",
+//   model: "RX-5",
+//   price: 50000,
+// });
+// console.log(myCar2);
+
+// const myCar3 = new Car({
+//   brand: "BMW",
+//   model: "X6",
+//   price: 55000,
+// });
+// console.log(myCar3);
+
+//***************************//
+
+// const User = function ({ email, password } = {}) {
+//   this.email = email;
+//   this.password = password;
+// };
+
+// // statik metod
+
+// User.logInfo = function (obj) {
+//   console.log(obj);
+// };
+// //////////////////
+
+// User.prototype.changeEmail = function (newEmail) {
+//   this.email = newEmail;
+// };
+
+// User.prototype.changePassword = function (newPassword) {
+//   this.password = newPassword;
+// };
+
+// const mango = new User({
+//   email: "mango@gmail.com",
+//   password: 111111,
+// });
+// console.log(mango);
+
+// mango.changeEmail("my-new-mail@gmail.com");
+
+// mango.changePassword(555555);
+
+// console.log(mango);
+
+// User.logInfo(mango);
